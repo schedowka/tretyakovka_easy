4bcfa5d4964740909fcd91dc7b4b6b06e6c97fec
schedowka <marina_molodkina@proton.me>
2024-12-24 20:47:13 +0300

    Initial commit

---
+++ src/Picture.java
@@ -0,0 +1,66 @@
+public class pictures {
+
+    private String name;
+    private String autor;
+    private int year;
+    private String country;
+
+    public pictures (String name; String autor; int year; int price; String country;) {
+        this.name = name;
+        this.autor = autor;
+        this.year = year;
+
+        this.country = country;
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+
+    }
+
+    public String getYear() {
+        return year;
+    }
+    public void setYear(int year) {
+        this.year = year;
+
+    }
+
+
+
+
+    public String getCountry() {
+        return country;
+    }
+    public void setCountry(String country) {
+        this.country = country;
+
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+       this.name = name;
+
+    }
+
+
+
+    @Override
+}
